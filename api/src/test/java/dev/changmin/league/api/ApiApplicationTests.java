fa7779a5b18bfb0fcf6ba547c4a37cebd7811c6c
changmink <changmin043@gmail.com>
2023-07-25 20:38:02 +0900

    [#3] 멀티 모듈 프로젝트로 변경

---
+++ api/src/test/java/dev/changmin/league/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.changmin.league.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
