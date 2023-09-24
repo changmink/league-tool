2e65312592e09e6e06210907e9b4ec7971736f35
changmink <changmin043@gmail.com>
2023-09-24 17:43:12 +0900

    [#3] MatchHandler 구현

--- api/src/main/java/dev/changmin/league/api/handler/MatchHandler.java
+++ api/src/main/java/dev/changmin/league/api/handler/MatchHandler.java
@@ -1,21 +1,35 @@
 package dev.changmin.league.api.handler;
 
+import dev.changmin.league.api.Repository;
+import dev.changmin.league.core.Game;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Component
 public class MatchHandler {
+    private final Repository repository;
+
+    public MatchHandler(Repository repository) {
+        this.repository = repository;
+    }
+
     public Mono<ServerResponse> get(ServerRequest serverRequest) {
-        return null;
+        String leagueId = serverRequest.pathVariable("league_id");
+        Map<String, Game> matches = repository.getMatches(leagueId);
+        if (null == matches) {
+            return ServerResponse.notFound().build();
+        }
+        return ServerResponse.ok().bodyValue(matches);
     }
 
     public Mono<ServerResponse> post(ServerRequest serverRequest) {
-        return null;
-    }
-
-    public Mono<ServerResponse> getById(ServerRequest serverRequest) {
-        return null;
+        String leagueId = serverRequest.pathVariable("league_id");
+        Map<String, Game> matches = repository.getLeague(leagueId).match();
+        repository.addMatches(leagueId, matches);
+        return ServerResponse.ok().bodyValue(matches);
     }
 }
